662a5d6d303e9a6e601d5459b2f25072aad98739
nahloo-obi <naluprosper54@gmail.com>
2024-04-28 11:13:16 +0100

    actions performance edit

--- jsactions/dist/pythonscript.py
+++ jsactions/dist/pythonscript.py
@@ -1,21 +1,40 @@
+import psutil
 import sys
 
-def main():
+
+def get_ram_usage():
+    try:
+        for process in psutil.process_iter(['pid', 'name']):
+            
+        # Check if the process name matches 'python' and 'manage.py'
+            if 'python' in process.info['name'] and 'manage.py' in process.info['name']:
+                # Get the PID of the Django process
+                django_pid = process.info['pid']
+        
+        input_value = sys.argv[1]
+        benchmarkValue = sys.argv[2]
+
+        if input_value == django_pid:
+            print("same process id")
+            return input_value
+        else:
+            return benchmarkValue
+
+        # print(f"Django Process PID: {django_pid}")
+        # # Get process object
+        # operation = psutil.Process(django_pid)
+
+        # # Get memory usage
+        # info_mem = operation.memory_info()
+        # usage_mem_mb = info_mem.rss / 1024 / 1024
+
+        # if usage_mem_mb > benchmarkValue:
+        #     raise ValueError(f"Analysis failed.")
+        # else:
+        #     return usage_mem_mb
+        
+    except psutil.NoSuchOperation:
+        raise ValueError(f"Process has discontinued.")
     
-    # Get the input argument
-    input_value = sys.argv[1]
-
-    # Your script logic goes here
-    result = your_script_logic(input_value)
-
-    # Print the result
-    print(result)
-    return result
-
-def your_script_logic(input_value):
-    # Your script logic here
-    # This is just an example; replace it with your actual script logic
-    return f"The input value must and will beee: {input_value}"
-
 if __name__ == "__main__":
-    main()
+    get_ram_usage()
--- jsactions/index.js
+++ jsactions/index.js
@@ -6,15 +6,49 @@ const path = require('path');
 
 try {
 
-const getValue = core.getInput('who-to-greet');
-const path_to_script = path.join(__dirname, 'scripts', 'pythonscript.py');
+const getBenchmarkValue = core.getInput('benchmark');
+
+ // Run the Django application using the manage.py script
+ const appCommand = 'python manage.py runserver 0.0.0.0:8000 &';
+ execSync(appCommand);
+
+ (async () => {
+ // Wait for a moment to ensure the Django app starts up
+ await new Promise(resolve => setTimeout(resolve, 5000));
+
+
+ // Get the process ID of the running Django application
+ const pidCommand = 'pgrep -f \'python manage.py runserver\'';
+ const pid = execSync(pidCommand).toString().trim();
+
+ const path_to_script = path.join(__dirname, 'scripts', 'pythonscript.py');
 
 // Execute the Python script with the input value using the shell command
-const getScriptResult = execSync(`python ${path_to_script} ${getValue}`).toString().trim();
+const getScriptResult = execSync(`python ${path_to_script} ${pid} ${getBenchmarkValue}`).toString().trim();
+
 console.log('Result of Python script:', getScriptResult);
 core.setOutput("scriptresult", getScriptResult);
+
 process.exit(0); // Exit with success status code
+})();
 
 } catch (error) {
   core.setFailed(error.message);
 }
+
+
+
+// try {
+
+// const getValue = core.getInput('who-to-greet');
+// const path_to_script = path.join(__dirname, 'scripts', 'pythonscript.py');
+
+// // Execute the Python script with the input value using the shell command
+// const getScriptResult = execSync(`python ${path_to_script} ${getValue}`).toString().trim();
+// console.log('Result of Python script:', getScriptResult);
+// core.setOutput("scriptresult", getScriptResult);
+// process.exit(0); // Exit with success status code
+
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
