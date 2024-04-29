6d2c45bcab4d6b61b279b1912bb922636e0e30f9
Weezeslim <chucksmacweeze@yahoo.com>
2024-04-29 13:57:22 +0100

    ram performance

--- jsactions/scripts/pythonscript.py
+++ jsactions/scripts/pythonscript.py
@@ -27,7 +27,7 @@ def get_ram_usage():
 
         #return ram_usage_list
         
-        if usage_mem_mb > BENCHMARK_VALUE:
+        if usage_mem_mb > int(BENCHMARK_VALUE):
             raise ValueError(f"Application ram usage exceeded benchmark.")
         else:
             return usage_mem_mb
